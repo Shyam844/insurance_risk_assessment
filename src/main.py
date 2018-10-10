2ede0ed3fe394bac6f6618df0683a69dcafb9cf3
Saketh Katari <katari15045@iiitd.ac.in>
2018-10-10 05:56:10 +0530

    elm : got 44.55 on Kaggle

--- src/main.py
+++ src/main.py
@@ -1,55 +1,29 @@
 import matplotlib
 matplotlib.use('Agg')
+from time import time
 from pandas import read_csv
 import numpy
-from time import time
-from sklearn.externals import joblib
-from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import StandardScaler
+import pickle
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
-from sklearn.preprocessing import StandardScaler
 from matplotlib import pyplot
+from sklearn.naive_bayes import GaussianNB
+from hpelm import ELM
 
+global tot_labels, clf
 
 start_time = time()
 
-def normalize_data(data):
-	scaler = StandardScaler()
-	return scaler.fit(data).transform(data)
+def init_global_vars():
+	global tot_labels
+	tot_labels = 8
 
 def read_csv_(path):
 	data_frame = read_csv(path)
 	handle_missing_values(data_frame)
 	data = data_frame.values
-	print("Raw Data -> " + str(data.shape))
 	return data
-
-
-def get_train_data(path):
-	# Assumption : The last column in CSV file is the label
-	# Assumption : The 3rd column of CSV file has strings
-	print("Getting Train Data...")
-	data = read_csv_(path)
-	rows, cols = data.shape
-	x = data[:, :cols-1]	# 'cols-1' is excluded
-	y = data[:, cols-1]	# only 'cols-1' column is retrieved
-	handle_str_col(x, 2)
-	x = numpy.asarray(x, dtype=numpy.float64)
-	y = numpy.asarray(y, dtype=numpy.int)
-	print("train_x : " + str(x.shape))
-	print("train_y : " + str(y.shape))
-	return x, y
-
-def get_test_data(path):
-	# Assumption : The 3rd column of CSV file has strings
-	print("Getting Test Data...")
-	data = read_csv_(path)
-	rows, cols = data.shape
-	x = data[:, :]
-	handle_str_col(x, 2)
-	x = numpy.asarray(x, dtype=numpy.float64)
-	print("test_x : " + str(x.shape))
-	return x
 
 def handle_missing_values(df):
 	df.fillna(df.mean(), inplace=True)
@@ -89,6 +63,36 @@ def save_results(test_x, pred_y):
 		count = count+1
 	book.close()	
 
+def normalize_data(data):
+	scaler = StandardScaler()
+	return scaler.fit(data).transform(data)
+
+def get_train_data(path):
+	# Assumption : The last column in CSV file is the label
+	# Assumption : The 3rd column of CSV file has strings
+	print("Getting Train Data...")
+	data = read_csv_(path)
+	rows, cols = data.shape
+	x = data[:, :cols-1]	# 'cols-1' is excluded
+	y = data[:, cols-1]	# only 'cols-1' column is retrieved
+	handle_str_col(x, 2)
+	x = numpy.asarray(x, dtype=numpy.float64)
+	y = numpy.asarray(y, dtype=numpy.int)
+	print("train_x : " + str(x.shape))
+	print("train_y : " + str(y.shape))
+	return x, y
+
+def get_test_data(path):
+	# Assumption : The 3rd column of CSV file has strings
+	print("Getting Test Data...")
+	data = read_csv_(path)
+	rows, cols = data.shape
+	x = data[:, :]
+	handle_str_col(x, 2)
+	x = numpy.asarray(x, dtype=numpy.float64)
+	print("test_x : " + str(x.shape))
+	return x
+
 def visualize_pca(x, y):
 	print("PCA...")
 	pca = PCA(n_components=2)
@@ -105,35 +109,94 @@ def visualize_tsne(x, y):
 	pyplot.scatter(x[:, 0], x[:, 1], c=y, s=10)
 	pyplot.savefig("vis_tsne.png")
 
-	
+def one_hot_encoding(data):
+	# Assumption: data is a 1-D Array
+	global tot_labels
+	encoded_data = numpy.zeros((len(data), tot_labels))
+	ind = 0
+	while(ind < len(data)):
+		num = data[ind]
+		encoded_data[ind][num-1] = 1
+		ind = ind+1
+	return encoded_data
+
+def one_hot_decoding_partial(data):
+	# Assumption: The shape of data should be same as that of "OneHotEncoding"
+	rows, cols = data.shape
+	row_ind = 0
+	decoded_data = numpy.zeros((data.shape))
+	while(row_ind < rows):
+		row = data[row_ind]
+		max_ind = numpy.argmax(row)
+		decoded_data[row_ind][max_ind] = 1
+		row_ind = row_ind+1
+	return decoded_data
+
+def one_hot_decoding_full(data):
+	# Assumption: The shape of data should be same as that of "OneHotEncoding"
+	# Returns 1-D array
+	rows, cols = data.shape
+	ind = 0
+	decoded_data = []
+	while(ind < rows):
+		row = data[ind]
+		max_ind = numpy.argmax(row)
+		decoded_data.append(max_ind+1)
+		ind = ind+1
+	return decoded_data	
+
+def elm(train_x, train_y, test_x):
+	global clf
+	print("ELM...")
+	global tot_labels
+	features = train_x.shape[1]
+	train_y = one_hot_encoding(train_y)
+	clf = ELM(features, tot_labels)
+	clf.add_neurons(250, "tanh")
+	clf.train(train_x, train_y, 'CV', 'OP', 'c', k=10)
+	pred_y = clf.predict(test_x)
+	pred_y = one_hot_decoding_full(pred_y)
+	return pred_y
+
+def save_model():
+	global clf
+	print("Saving Model...")
+	f_ = open("model.pkl", "wb")
+	pickle.dump(clf, f_)
+	f_.close()
+
 def main():
+	global clf
+	init_global_vars()
 	# Get Data
 	train_x, train_y = get_train_data("../dataset/train.csv")
-	test_x = get_test_data("../dataset/test.csv")
+	test_x_raw = get_test_data("../dataset/test.csv")
 
 	# Normalize Data
 	train_x = normalize_data(train_x)
-	test_x = normalize_data(test_x)
+	test_x = normalize_data(test_x_raw)
 
+	pred_y = elm(train_x, train_y, test_x)
+	clf.save("model.pkl")
+
+	# Save Results
+	save_results(test_x_raw, pred_y)
+
+'''
 	# Visualize Data
 	visualize_pca(train_x, train_y)
 	visualize_tsne(train_x, train_y)
-'''
+
 	# Train the Model
 	clf = GaussianNB()
 	print("Training...")
 	clf.fit(train_x, train_y)
 
-	# Save the Model
-	print("Saving Model...")
-	joblib.dump(clf, "model.pkl")
-
 	# Test the Model
 	print("Testing...")
 	pred_y = clf.predict(test_x)
 	
-	# Save Results
-	save_results(test_x, pred_y)'''
+	'''
 
 main()
 
